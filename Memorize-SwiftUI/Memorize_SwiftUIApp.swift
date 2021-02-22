0836aa00364fdb752e35537fb197f0ff3b6b5723
Eyup Cimen <cmneyp@gmail.com>
2021-02-23 00:57:00 +0300

    First Commit

---
+++ Memorize-SwiftUI/Memorize_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_SwiftUIApp.swift
+//  Memorize-SwiftUI
+//
+//  Created by eyup cimen on 23.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
