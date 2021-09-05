bd2f8f0df9a2d77cde2aa641a435b4dbbebf6394
eyupcimenipucu <eyupcimen@ipucubilisim.com.tr>
2021-09-05 21:32:31 +0300

    no message

--- Memorize-SwiftUI/ContentView.swift
+++ Memorize-SwiftUI/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            ZStack{
+                RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                    .stroke(lineWidth: 3.0)
+                Text("Hello World!")
+                    .padding()
+            }
+            ZStack{
+                RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                    .stroke(lineWidth: 3.0)
+                Text("Hello World!")
+                    .padding()
+            }
+            ZStack{
+                RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                    .stroke(lineWidth: 3.0)
+                Text("Hello World!")
+                    .padding()
+            }
+            ZStack{
+                RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                    .stroke(lineWidth: 3.0)
+                Text("Hello World!")
+                    .padding()
+            }
+        }
+            .padding(.horizontal)
+            .foregroundColor(.orange)
+            
+            //NavigationView {
+            //.navigationTitle(Text("Standford SwiftUI"))
+            //.navigationBarTitleDisplayMode(.large)
+      
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+// .edgesIgnoringSafeArea(.bottom)
+//Button("Show Menu") {
+//    showingPopover = true
+//}
+//.popover(isPresented: $showingPopover) {
+//    VStack {
+//        Section{
+//            Text("Your content here")
+//                .font(.headline)
+//                .padding()
+//            Text("Your content here")
+//                .font(.headline)
+//                .padding()
+//        }
+//
+//    }
+//
+//}
